53aee686c4d4138d78251771290047a89990d3ae
Alan Ogilvie <alan.ogilvie@gmail.com>
2026-09-02 23:39:21 +0100

    fix(companion): force native NSWindow transparency in swift

--- companion_app/macos/Runner/MainFlutterWindow.swift
+++ companion_app/macos/Runner/MainFlutterWindow.swift
@@ -7,6 +7,15 @@ class MainFlutterWindow: NSWindow {
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+    
+    // Force transparency for borderless popup
+    self.isOpaque = false
+    self.backgroundColor = NSColor.clear
+    self.styleMask.insert(.fullSizeContentView)
+    self.styleMask.insert(.borderless)
+    
+    // Ensure the Flutter view itself has a clear background
+    flutterViewController.backgroundColor = NSColor.clear
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
