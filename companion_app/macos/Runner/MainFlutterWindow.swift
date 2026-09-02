f6af819ab235881e7b9b59df0b8ac2e6acf25a5b
Alan Ogilvie <alan.ogilvie@gmail.com>
2026-09-02 23:47:17 +0100

    fix(companion): spawn native window off-screen to prevent launch flash

--- companion_app/macos/Runner/MainFlutterWindow.swift
+++ companion_app/macos/Runner/MainFlutterWindow.swift
@@ -4,7 +4,9 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
+    var windowFrame = self.frame
+    // Spawn off-screen to prevent the brief black flash on launch!
+    windowFrame.origin = NSPoint(x: -10000, y: -10000)
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
     
