a5b24e6bac34f4bc08913944ff69d7bda8ddd5c4
Alan Ogilvie <alan.ogilvie@gmail.com>
2026-09-04 22:08:00 +0100

    chore: remove scratch files accidentally tracked

--- extension/test-ha.mjs
+++
@@ -1,6 +0,0 @@
-import WebSocket from 'ws';
-
-const haUrl = "http://homeassistant.local:8123";
-const haToken = "YOUR_TOKEN"; // Wait, I need the actual token!
-
-// I don't have the token. I can't easily query HA from a raw script without the token.
--- extension/test_storage.ts
+++
@@ -1,6 +0,0 @@
-import { LocalStorage } from "@raycast/api";
-
-export default async function Command() {
-    await LocalStorage.setItem("test", "hello");
-    console.log(await LocalStorage.getItem("test"));
-}
